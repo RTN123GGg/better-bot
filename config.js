400d82d0345410bcbbf09a736989640f771f0e12
RTN123GGg <rioinvesting9@gmail.com>
2025-01-28 03:20:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ module.exports = {
     "registercommands" : false, //Write True If You Are Launching The Bot First Time
      "token": "", //Your Super Secret Bot Token
     "imageapi": "", //Your Amethyste Api You can get it from https://api.amethyste.moe/
-    "ownerID": [], //Your Discord User ID
-"prefix": "", //Your Bot's Prefix
+    "ownerID": [793278611112329276], //Your Discord User ID
+"prefix": ".", //Your Bot's Prefix
 "chat": {
         "url": "",
-        "bid": "",
+        "bid": "1332912122262323340",
         "key": "",
         "uid": ""
     }, // You can get This Things from https://brainshop.ai
 "api": "", // Your Youtube Api
 "youtubeAPI": "", // Your Youtube Api
-mainprefix: "",  // Again Your Prefix
-"owner": "G U D B O Y", // Owner Name
+mainprefix: ".",  // Again Your Prefix
+"owner": "! ß13 🇹🇳 RIO", // Owner Name
 
   basiclang: "en", //The basic language of the bot, "fr" for French and "en" for English
     embeds: {
